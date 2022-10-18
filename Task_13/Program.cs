870df2e59bd21413010aa1b4b03ad3aef914fd03
Katy <bev08021982@gmail.com>
2022-10-19 01:01:59 +0400

    Задача 10 впроцессе

--- Task_13/Program.cs
+++ Task_13/Program.cs
@@ -2,3 +2,20 @@
 645 -> 5
 78 -> третьей цифры нет
 32679 -> 6 */
+
+//Функции 
+int SearchthirdDigit(int number)
+{
+    int firstDigit = (number / 100) * 100;
+    int thirdDigit = number % 100;
+    int secondDigit = ((number - firstDigit - thirdDigit)/10);
+    return thirdDigit;
+}
+
+//Тело программы
+
+Console.WriteLine("Введите целое положительное число:");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int thirdDigit = SearchthirdDigit(num);
+Console.Write($"Вторая цифра числа {num}: {thirdDigit}");

ef4d4f4897d2e2d69de4f9d788bd2fb98792b193
Katy <bev08021982@gmail.com>
2022-10-20 20:50:28 +0400

    Доделала 10 задачу проверка больше 999.

--- Task_10/Program.cs
+++ Task_10/Program.cs
@@ -1,33 +1,34 @@
-﻿/* Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+﻿/* Задача 10: Напишите программу, которая принимает на вход трёхзначное число и 
+на выходе показывает вторую цифру этого числа.
 456 -> 5
 782 -> 8
 918 -> 1
 */
 
-//Функция
+//Функция расчета третьего числа.
 int SearchsecondDigit(int number)
 {
     int firstDigit = (number / 100) * 100;
     int thirdDigit = number % 10;
-    int secondDigit = ((number - firstDigit - thirdDigit)/10);
+    int secondDigit = ((number - firstDigit - thirdDigit) / 10);
     return secondDigit;
 }
 
-
-//Тело Программы
+//Программа
 
 Console.WriteLine("Введите трехзначное целое положительное число:");
 int num = Convert.ToInt32(Console.ReadLine());
 
-if ((num / 100) > 0)
+//Проверка на трехзначное положителное.
+if ((num / 100) > 0 && ((num / 1000) == 0))
 {
-Console.WriteLine ($"Вы вели число {num}.");
+    Console.WriteLine($"Вы вели число {num}.");
 }
 else
 {
     Console.WriteLine("Не верно, нужно ввести трехзначное число.");
     return;
-} 
+}
 
 int secondDigit = SearchsecondDigit(num);
 Console.Write($"Вторая цифра числа {num}: {secondDigit}");
